7d984b0fc59cd9c9d88f2e9c05b281a3dd65b26e
Martín Sande Costa <martin.sande@udc.es>
2023-02-24 15:29:36 +0100

    feature(1): Project skeleton

---
+++ PlayTime/PlayTime/PlayTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayTimeApp.swift
+//  PlayTime
+//
+//  Created by Martín Sande on 24/2/23.
+//
+
+import SwiftUI
+
+@main
+struct PlayTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
